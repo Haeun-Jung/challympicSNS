bd0c5644867b4cd4356950cd9d147356287489e4
곽현준 <kwak0568@naver.com>
2022-01-26 11:55:17 +0900

    feat<BE> 백엔드 커밋

---
+++ BACKEND/src/main/java/com/ssafy/challympic/ChallympicApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.challympic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallympicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallympicApplication.class, args);
+	}
+
+}
---
+++ BACKEND/src/test/java/com/ssafy/challympic/ChallympicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.challympic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallympicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
