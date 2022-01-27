6238a84b8e58c54e76ac8e64711ecdb5b378f286
june_25 <hp0006@mix.wvu.edu>
2022-01-27 14:03:14 +0900

    feat<FE> : 사이드 메뉴 및 내용 추가

---
+++ FRONTEND/src/components/layout/Side.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card
+    max-width="20%"
+    class="hidden-sm-and-down"
+    elevation="0"
+    height="100vh"
+  >
+    <side-contents />
+  </v-card>
+</template>
+
+<script>
+import SideContents from "../layout/SideContents.vue";
+
+export default {
+  name: "Side",
+  components: { SideContents },
+
+  data() {
+    return {
+      user: false,
+    };
+  },
+};
+</script>
+<!--
+<style lang="scss" scoped>
+ @import '~vuetify/src/components/VStepper/_variables.scss';
+$navigation-drawer-border-width :20px;
+</style>-->
