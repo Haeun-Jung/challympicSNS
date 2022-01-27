ad6c30e40b9c90449d221336b0cfb2735e371180
june_25 <hp0006@mix.wvu.edu>
2022-01-27 14:02:22 +0900

    feat<FE>: 메인 페이지 추가

---
+++ FRONTEND/src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-main>
+    <tool-bar />
+    <tag-bar />
+    <side />
+    <fab-button />
+    <!-- 리스트 공간
+  -->
+  </v-main>
+</template>
+
+<script>
+import ToolBar from "../components/layout/ToolBar.vue";
+import TagBar from "../components/layout/TagBar.vue";
+import Side from "../components/layout/Side.vue";
+import FabButton from "../components/button/FabButton.vue";
+export default {
+  components: { ToolBar, TagBar, Side, FabButton },
+};
+</script>
+
+<style></style>
