7f404468bf040a588a9b0b55a52c8916b46f188a
mindu23 <mindj5541@google.com>
2022-01-30 16:47:44 +0900

    feat<BE>: userApi 구현 - File, Interest, Tag, User 테이블 생성

---
+++ BACKEND/src/main/java/com/ssafy/challympic/service/UserService.java
@@ -0,0 +1,81 @@
+package com.ssafy.challympic.service;
+
+import com.ssafy.challympic.domain.User;
+import com.ssafy.challympic.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 이메일 중복 감지
+     */
+    public boolean validateDuplicateEmail(String user_email){
+        List<User> findUser = userRepository.validateEmail(user_email);
+        if(findUser.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 닉네임 중복 감지
+     */
+    public boolean validateDuplicateNickname(String user_nickname){
+        List<User> findUser = userRepository.validateNickname(user_nickname);
+        if(findUser.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public int join(User user){
+        userRepository.save(user);
+        return user.getUser_no();
+    }
+
+    /**
+     * 로그인
+     */
+    public User login(String user_email, String user_pwd){
+        // no, nickname, title
+        return userRepository.login(user_email, user_pwd);
+    }
+
+    /**
+     * 정보 수정
+     */
+    @Transactional
+    public void updateNickname(int user_no, String user_nickname){
+        User user = userRepository.findOne(user_no);
+        user.setUser_nickname(user_nickname);
+    }
+
+    @Transactional
+    public void updatePwd(int user_no, String user_pwd){
+        User user = userRepository.findOne(user_no);
+        user.setUser_pwd(user_pwd);
+    }
+
+    public User userInfo(int user_no){
+        return userRepository.findOne(user_no);
+    }
+
+    public void deleteUser(int user_no) {
+        userRepository.delete(user_no);
+    }
+
+    public void findUser(String user_nickname){ userRepository.validateNickname(user_nickname); }
+}
