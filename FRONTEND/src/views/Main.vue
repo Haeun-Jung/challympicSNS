e30b8faa8b4a6eb152cd65a73bf22e998761c28f
june_25 <hp0006@mix.wvu.edu>
2022-01-31 22:03:57 +0900

    style<FE> : 프리티어 적용

--- FRONTEND/src/views/Main.vue
+++ FRONTEND/src/views/Main.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-main>
-    <tool-bar />
-    <tag-bar />
-    <side />
-    <!-- 리스트 공간 라우터 뷰 필요 
+	<v-main>
+		<tool-bar />
+		<tag-bar />
+		<side />
+		<!-- 리스트 공간 라우터 뷰 필요 
   -->
-  </v-main>
+	</v-main>
 </template>
 
 <script>
-import ToolBar from "../components/layout/ToolBar.vue";
-import TagBar from "../components/layout/TagBar.vue";
-import Side from "../components/layout/Side.vue";
+	import ToolBar from "../components/layout/ToolBar.vue";
+	import TagBar from "../components/layout/TagBar.vue";
+	import Side from "../components/layout/Side.vue";
 
-export default {
-  components: { ToolBar, TagBar, Side },
-};
+	export default {
+		components: { ToolBar, TagBar, Side },
+	};
 </script>
 
 <style></style>
