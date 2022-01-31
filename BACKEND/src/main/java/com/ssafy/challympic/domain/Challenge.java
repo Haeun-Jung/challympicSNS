c29f9e7f696d4e6ac8ed6dbf48d693c2f24a8c37
곽현준 <kwak0568@naver.com>
2022-01-31 09:54:13 +0900

    fix<BE>: BE 브랜치 병합 중 오류 해결

--- BACKEND/src/main/java/com/ssafy/challympic/domain/Challenge.java
+++ BACKEND/src/main/java/com/ssafy/challympic/domain/Challenge.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -20,8 +21,14 @@ public class Challenge {
     @JoinColumn(name = "user_no")
     private int user_no;
 
-    private LocalDateTime challenge_start;
+    /**
+     * temporal 오류
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP default CURRENT_TIMESTAMP")
+    private Date challenge_start;
 
+//    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime challenge_end;
 
     private String challenge_access;
@@ -32,8 +39,10 @@ public class Challenge {
 
     private String challenge_content;
 
+    @Column(columnDefinition = "default false")
     private boolean challenge_official;
 
+    @Column(columnDefinition = "default 0")
     private int challenge_report;
 
 //    @OneToOne(fetch = LAZY)
@@ -41,7 +50,7 @@ public class Challenge {
     private int title_no;
 
     // 생성 메소드
-    public static Challenge createChallenge(int user_no, LocalDateTime challenge_start, LocalDateTime challenge_end, String challenge_access, String challenge_type, String challenge_title, String challenge_content) {
+    public static Challenge createChallenge(int user_no, Date challenge_start, LocalDateTime challenge_end, String challenge_access, String challenge_type, String challenge_title, String challenge_content) {
         Challenge challenge = new Challenge();
         challenge.setUser_no(user_no);
         challenge.setChallenge_start(challenge_start);
