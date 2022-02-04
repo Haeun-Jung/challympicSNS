86edd5a4403e377f41c1c0d01adabd850bb3ce0c
yujin won <won_yujin@naver.com>
2022-02-04 16:05:32 +0900

    chore<FE>: 참여하기 클릭 시 챌린지명 prop 전달하도록 수정

--- FRONTEND/src/components/upload/PostUpload.vue
+++ FRONTEND/src/components/upload/PostUpload.vue
@@ -1,26 +1,31 @@
 <template>
   <!-- POST 등록 모달 -->
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-card>
-        <v-card-title class="card-header"> 
-          <v-spacer></v-spacer>
-          <v-btn class="cancel-btn" icon @click="$emit('close-modal')"> 
-            <img class="cancel-icon" src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-cancel-mintab-for-ios-becris-lineal-becris.png"/>
-          </v-btn>
-        </v-card-title>
-        <v-card-text>
-          <v-row>
-            <!-- 업로드 공간 -->
-            <v-col cols="12" sm="12" md="12" class="upload-area">
-              <div class="example-drag">
-              <div v-show="$refs.upload && $refs.upload.dropActive" class="drop-active">
-              </div>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-card>
+      <v-card-title class="card-header">
+        <v-spacer></v-spacer>
+        <v-btn class="cancel-btn" icon @click="$emit('close-modal')">
+          <img
+            class="cancel-icon"
+            src="https://img.icons8.com/external-becris-lineal-becris/64/000000/external-cancel-mintab-for-ios-becris-lineal-becris.png"
+          />
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <!-- 업로드 공간 -->
+          <v-col cols="12" sm="12" md="12" class="upload-area">
+            <div class="example-drag">
+              <div
+                v-show="$refs.upload && $refs.upload.dropActive"
+                class="drop-active"
+              ></div>
               <template v-if="files.length">
-                <v-data-table 
-                  dense 
-                  :headers="headers" 
-                  :items="files" 
-                  item-key="name" 
+                <v-data-table
+                  dense
+                  :headers="headers"
+                  :items="files"
+                  item-key="name"
                   class="elevation-1"
                   hide-default-footer
                 >
@@ -40,13 +45,17 @@
                 <v-row>
                   <v-col cols="12" sm="12" md="12">
                     <div class="text-center p-5">
-                        <div v-if="this.type === 'register'" class="start-challenge-head">🎊챌린지에 처음으로 도전하세요🎊</div>
-                        <v-btn
-                          class="ma-2"
-                          color="#3396F4"
+                      <div
+                        v-if="this.type === 'register'"
+                        class="start-challenge-head"
+                      >
+                        🎊챌린지에 처음으로 도전하세요🎊
+                      </div>
+                      <v-btn class="ma-2" color="#3396F4">
+                        <label for="file" class="upload-btn"
+                          >파일 업로드하기</label
                         >
-                          <label for="file" class="upload-btn">파일 업로드하기</label>
-                        </v-btn>
+                      </v-btn>
                       <h5>최대 업로드 파일 크기 : 50MB</h5>
                       <h5>사진 : JPG, JPEG, PNG</h5>
                       <h5>영상 : MP4, AVI</h5>
@@ -55,57 +64,64 @@
                 </v-row>
               </template>
             </div>
-            </v-col>  
-          </v-row>
-          <!-- 본문 -->
-          <v-row>
-            <v-col class="input-title"># 챌린지 선택</v-col>
-          </v-row>
-          <v-row  class="row-area">
-            <v-col
-              class="d-flex"
+          </v-col>
+        </v-row>
+        <!-- 본문 -->
+        <v-row>
+          <v-col class="input-title"># 챌린지 선택</v-col>
+        </v-row>
+        <v-row class="row-area">
+          <v-col class="d-flex">
+            <v-text-field
+              v-if="challengeName"
+              :value="`${challengeName}`"
+              disabled
+              outlined
+            ></v-text-field>
+            <v-select
+              v-else
+              v-model="challenge"
+              :items="challenges"
+              dense
+              outlined
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row class="row-area">
+          <v-col class="input-title"># 참여파일형식</v-col>
+          <v-col v-model="form" class="media-form">사진/동영상</v-col>
+        </v-row>
+        <v-row>
+          <v-col class="input-title"># 내용(최대 255자)</v-col>
+        </v-row>
+        <v-row class="row-area">
+          <v-col>
+            <v-textarea
+              v-model="description"
+              outlined
+              name="input-7-4"
+              placeholder="운동하니까 힘들다 #운동 @김싸피"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-spacer></v-spacer>
+          <span class="error-text" v-if="error">{{ error }}</span>
+          <v-btn class="ma-2" color="#3396F4">
+            <label
+              v-if="this.type === 'register'"
+              class="upload-btn"
+              @click="uploadStart()"
+              >챌린지 등록하기</label
             >
-              <v-select
-                v-model="challenge"
-                :items="challenges"
-                dense
-                outlined
-              ></v-select>
-            </v-col>
-          </v-row>
-          <v-row class="row-area">
-            <v-col class="input-title"># 참여파일형식</v-col>
-            <v-col v-model="form" class="media-form">사진/동영상</v-col>
-          </v-row>
-          <v-row>
-            <v-col class="input-title"># 내용(최대 255자)</v-col>
-          </v-row>
-          <v-row class="row-area">
-            <v-col
-                >
-              <v-textarea
-                v-model="description"
-                outlined
-                name="input-7-4"
-                placeholder="운동하니까 힘들다 #운동 @김싸피"
-              ></v-textarea>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-spacer></v-spacer>
-            <span class="error-text" v-if="error">{{ error }}</span>
-            <v-btn
-                class="ma-2"
-                color="#3396F4"
-              >
-                <label v-if="this.type === 'register'" class="upload-btn" @click="uploadStart()">챌린지 등록하기</label>
-                <label v-else class="upload-btn" @click="uploadStart()">참여하기</label>
-              </v-btn>
-          </v-row>
-          
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+            <label v-else class="upload-btn" @click="uploadStart()"
+              >참여하기</label
+            >
+          </v-btn>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -114,43 +130,48 @@ import FileUpload from "vue-upload-component";
 export default {
   name: "PostUpload",
   components: {
-    FileUpload
+    FileUpload,
   },
   props: {
     type: { type: String },
+    challengeName: { type: String },
   },
-  data: ()=> ({
+  data: () => ({
     dialog: true, //true : Dialog열림, false : Dialog닫힘
-    challenges: ['미라클_모닝_챌린지', '싸피_챌린지'],
-    file: '',
-    challenge: '',
-    form: '',
-    description: '',
+    challenges: ["미라클_모닝_챌린지", "싸피_챌린지"],
+    file: "",
+    challenge: "",
+    form: "",
+    description: "",
     error: false,
     files: [],
     headers: [
-      { text: 'name', value: 'name' },
-      { text: 'size', value: 'size' },
+      { text: "name", value: "name" },
+      { text: "size", value: "size" },
     ],
   }),
   methods: {
-    onDrop(item){
-      console.log(item)
+    onDrop(item) {
+      console.log(item);
     },
     uploadStart() {
-        // 업로드할 파일 형식과 참여파일형식이 맞지 않을 때 확인(file, form)
+      // 업로드할 파일 형식과 참여파일형식이 맞지 않을 때 확인(file, form)
 
-        // 항목들 입력 여부 확인
-        if (this.file.length == 0 || this.challenge.length == 0 || this.description.length == 0) {
-            this.error = "입력되지 않은 항목이 있습니다."
-        } else {
-            this.error = false;
-        }
+      // 항목들 입력 여부 확인
+      if (
+        this.file.length == 0 ||
+        this.challenge.length == 0 ||
+        this.description.length == 0
+      ) {
+        this.error = "입력되지 않은 항목이 있습니다.";
+      } else {
+        this.error = false;
+      }
 
-        // 업로드 로직
-    }
-  }
-}
+      // 업로드 로직
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -167,13 +188,13 @@ export default {
   left: 0;
   position: fixed;
   z-index: 9999;
-  opacity: .6;
+  opacity: 0.6;
   text-align: center;
-  width:100%;
+  width: 100%;
   background: #000;
 }
 .example-drag .drop-active h3 {
-  margin: -.5em 0 0;
+  margin: -0.5em 0 0;
   position: absolute;
   top: 50%;
   left: 0;
@@ -186,11 +207,11 @@ export default {
   padding: 0;
 }
 .cancel-btn {
-    margin-top: -10px;
-    margin-right: -20px;
+  margin-top: -10px;
+  margin-right: -20px;
 }
 .cancel-icon {
-    width: 16px;
+  width: 16px;
 }
 .upload-area {
   position: relative;
@@ -203,11 +224,11 @@ export default {
   cursor: pointer;
 }
 .start-challenge-head {
-  color: #3396F4;
+  color: #3396f4;
   margin-bottom: 20px;
 }
 .input-title {
-  color: #3396F4;
+  color: #3396f4;
 }
 .row-area {
   margin-top: -20px;
@@ -217,9 +238,9 @@ export default {
   text-align: right;
 }
 .error-text {
-    display: flex;
-    color: rgb(235, 38, 38);
-    align-items: center;
-    margin-right: 14px;
+  display: flex;
+  color: rgb(235, 38, 38);
+  align-items: center;
+  margin-right: 14px;
 }
 </style>
