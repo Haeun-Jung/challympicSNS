a695ce6009878fa35712c3d378bf0936b88c3277
yujin won <won_yujin@naver.com>
2022-02-04 16:01:07 +0900

    feat<FE>: 포스트 이미지 및 동영상 컴포넌트 추가

---
+++ FRONTEND/src/components/post/PostImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-img
+      height="500"
+      contain
+      class="grey darken-4"
+      :src="require(`@/assets/${fileName}`)"
+    ></v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostImage",
+  props: {
+    fileName: String,
+  },
+};
+</script>
+
+<style>
+</style>
