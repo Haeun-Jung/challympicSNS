268c36cb24d548f4f08848e8ff5a59f4416a5138
kwonohbeom <xpzmfhf@gmail.com>
2022-02-06 17:06:19 +0900

    feat<BE>: Post Like API 추가

--- BACKEND/src/main/java/com/ssafy/challympic/repository/PostLikeRepository.java
+++ BACKEND/src/main/java/com/ssafy/challympic/repository/PostLikeRepository.java
@@ -1,11 +1,16 @@
 package com.ssafy.challympic.repository;
 
+import com.ssafy.challympic.domain.Post;
 import com.ssafy.challympic.domain.PostLike;
+import com.ssafy.challympic.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,17 +18,29 @@ public class PostLikeRepository {
 
     private final EntityManager em;
 
-    @Transactional(readOnly = true)
-    public PostLike findPostLikeByUserNoPostNo(int postNo, int userNo){
-        return em.createQuery("select ps from PostLike ps where ps.user_no = :userNo and ps.post_no = :postNo", PostLike.class)
-                .setParameter("userNo", userNo)
+    @Transactional
+    public List<PostLike> findByPostNo(Integer postNo){
+        return em.createQuery("select pl from PostLike pl where pl.post_no = :postNo", PostLike.class)
                 .setParameter("postNo", postNo)
-                .getSingleResult();
+                .getResultList();
     }
 
     @Transactional
-    public void deleteByPostNoUserNo(int postNo, int userNo){
-        em.remove(findPostLikeByUserNoPostNo(postNo, userNo));
+    public List<PostLike> findPostLikeByUserNoPostNo(Integer postNo, Integer userNo){
+
+        List<PostLike> postLike = em.createQuery("select pl from PostLike pl where pl.post_no = :postNo and pl.user_no = :userNo", PostLike.class)
+                .setParameter("postNo", postNo)
+                .setParameter("userNo", userNo)
+                .getResultList();
+        return postLike;
+    }
+
+    @Transactional
+    public void deleteByPostUser(Integer postNo, Integer userNo){
+        List<PostLike> postLikeList = findPostLikeByUserNoPostNo(postNo, userNo);
+        for(PostLike postLike : postLikeList){
+            em.remove(postLike);
+        }
     }
 
     @Transactional
