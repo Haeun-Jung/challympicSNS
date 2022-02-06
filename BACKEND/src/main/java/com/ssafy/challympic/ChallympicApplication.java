e5135ddb20ded63038f7f2e15e14601af65378a6
kwonohbeom <xpzmfhf@gmail.com>
2022-02-06 17:04:24 +0900

    build<BE>: aws.yml 반영

--- BACKEND/src/main/java/com/ssafy/challympic/ChallympicApplication.java
+++ BACKEND/src/main/java/com/ssafy/challympic/ChallympicApplication.java
@@ -3,12 +3,21 @@ package com.ssafy.challympic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ChallympicApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
+			"classpath:application.yml, " +
+			"classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(ChallympicApplication.class, args);
+		new SpringApplicationBuilder(ChallympicApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+						.run(args);
+
+//		SpringApplication.run(ChallympicApplication.class, args);
 	}
 
 }
