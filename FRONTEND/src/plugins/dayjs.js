9da23dd563c08854f3f6e4393187149b2a3b6e63
yujin won <won_yujin@naver.com>
2022-02-07 16:47:59 +0900

    build<FE>: dayjs 설치 및 설정 파일 추가

---
+++ FRONTEND/src/plugins/dayjs.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+const fromNow = (dateTime) => {
+  const diff = dayjs().diff(dateTime,'days');
+  console.log(dayjs(dateTime).fromNow());
+  if (diff < 7) {
+    return dayjs(dateTime).fromNow();
+  }
+  return dayjs(dateTime).format("YYYY-MM-DD");
+}
+
+export default fromNow;
