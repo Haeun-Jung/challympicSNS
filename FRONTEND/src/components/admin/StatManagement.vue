f271398b518a6b8ee69a6afdc3f49a98eeee31df
june_25 <hp0006@mix.wvu.edu>
2022-02-08 16:06:47 +0900

    build<FE> : 현황 및 통계 페이지 생성

---
+++ FRONTEND/src/components/admin/StatManagement.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-card height="85vh" elevation="0">
+		<v-card-title> 현황 · 통계</v-card-title>
+		<v-divider />
+		<stat-table />
+	</v-card>
+</template>
+
+<script>
+	import StatTable from "./util/StatTable.vue";
+	export default {
+		name: "StatManagement",
+		components: { StatTable },
+	};
+</script>
+
+<style></style>
