74b32f01267d9f63ce5c145a771122e1651f908f
june_25 <hp0006@mix.wvu.edu>
2022-02-09 00:58:42 +0900

    feat<FE> : 관리자 페이지 통계 페이지 구현중

--- FRONTEND/src/components/admin/StatManagement.vue
+++ FRONTEND/src/components/admin/StatManagement.vue
@@ -2,15 +2,52 @@
 	<v-card height="85vh" elevation="0">
 		<v-card-title> 현황 · 통계</v-card-title>
 		<v-divider />
-		<stat-table />
+
+		<v-conatiner fluid>
+			<v-layout row>
+				<v-flex xs5>
+					<!-- 왼쪽 콜륨 -->
+					<v-layout column>
+						<v-container fluid>
+							<v-list-item-title class="ma-2">
+								<v-icon color="#3396F4">mdi-trophy-variant</v-icon>인기 챌린지
+								목록
+							</v-list-item-title>
+							<v-layout row>
+								<popular-challenge-table />
+							</v-layout>
+							<v-layout row>
+								<v-list-item-title class="ma-2">
+									<v-icon color="#3396F4">mdi-comment-question</v-icon>
+									답변이 필요한 문의
+								</v-list-item-title>
+								<popular-challenge-table />
+							</v-layout>
+						</v-container>
+					</v-layout>
+				</v-flex>
+
+				<!-- 오른쪽 콜륨 -->
+				<v-flex xs6>
+					<v-layout column>
+						<v-list-item-title class="ma-2">
+							<v-icon color="#3396F4">mdi-comment-question</v-icon>
+							답변이 필요한 문의
+						</v-list-item-title>
+						<unanswered-table />
+					</v-layout>
+				</v-flex> </v-layout
+			><!-- end of row -->
+		</v-conatiner>
 	</v-card>
 </template>
 
 <script>
-	import StatTable from "./util/StatTable.vue";
+	import PopularChallengeTable from "./util/PopularChallengeTable.vue";
+	import UnansweredTable from "./util/UnansweredTable.vue";
 	export default {
 		name: "StatManagement",
-		components: { StatTable },
+		components: { PopularChallengeTable, UnansweredTable },
 	};
 </script>
 
