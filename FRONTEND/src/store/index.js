ddf0d966d68fbbf107574e4162eef68ff21deb8e
yujin won <won_yujin@naver.com>
2022-02-10 17:47:02 +0900

    feat<FE>: 챌린지 생성, 챌린지 가져오기, 챌린지명 중복확인 API 연결

--- FRONTEND/src/store/index.js
+++ FRONTEND/src/store/index.js
@@ -5,6 +5,8 @@ import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 import userStore from '@/store/modules/userStore.js'
+import postStore from '@/store/modules/postStore.js'
+import challengeStore from '@/store/modules/challengeStore.js'
 
 export default new Vuex.Store({
   state: {},
@@ -12,6 +14,8 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     userStore,
+    postStore,
+    challengeStore
   },
   plugins: [
     createPersistedState({
