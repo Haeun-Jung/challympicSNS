596f6e16ef628d7ec4ef3fd770fef025e3d664ba
yujin won <won_yujin@naver.com>
2022-02-11 11:46:02 +0900

    fix<FE>: 챌린지명 중복확인, 챌린지/포스트 생성 api 통신 관련 에러 해결

--- FRONTEND/src/api/challenge.js
+++ FRONTEND/src/api/challenge.js
@@ -6,12 +6,12 @@ function getChallenge(challengeNo, success, fail) {
   api.get(`/challympic/challenge/${challengeNo}`).then(success).catch(fail);
 }
 
-function confirmChallengeName(challengeName, success, fail) {
-  api.post('/challympic/confirm', JSON.stringify(challengeName)).then(success).catch(fail);
+function confirmChallengeName(challenge_title, success, fail) {
+  api.post('/challympic/challenge/confirm', JSON.stringify({ challenge_title })).then(success).catch(fail);
 }
 
-function createChallenge(challenge, success, fail) {
-  api.get('/challympic/challenge', JSON.stringify(challenge)).then(success).catch(fail);
+async function createChallenge(challenge, success, fail) {
+  await api.get('/challympic/challenge', JSON.stringify(challenge)).then(success).catch(fail);
 }
 
 export { getChallenge, confirmChallengeName, createChallenge };
