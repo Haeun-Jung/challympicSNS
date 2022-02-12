e44005f363cce8e44bff8918badd6c442ccb688e
june_25 <hp0006@mix.wvu.edu>
2022-02-13 06:40:47 +0900

    fix<FE> : 포스트 및 챌린지 검색 결과 이슈 수정 1. m3u8 파일 확장자 사용 시 src 가 바인딩 안되는 문제를 created로 처리했습니다. 2. responsive video size로 만듦

--- FRONTEND/src/components/post/Player.vue
+++ FRONTEND/src/components/post/Player.vue
@@ -1,36 +1,35 @@
 <template>
-  <div>
-    <video-player
-      class="video-player-box"
-      ref="videoPlayer"
-      :playsinline="true"
-      :options="playerOptions"
-    ></video-player>
-  </div>
+	<div>
+		<video-player
+			class="video-player-box"
+			ref="videoPlayer"
+			:playsinline="true"
+			:options="playerOptions"
+		></video-player>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Player",
-  data() {
-    return {
-      playerOptions: {
-        preload: "auto",
-        autoplay: true,
-        muted: true,
-        loop: true,
-        aspectRatio: "4:3",
-        sources: [
-          {
-            type: "application/x-mpegURL",
-            src: "http://d384sk7z91xokb.cloudfront.net/output/video/2022.02.06/c88b277171a94d9c3ac7e32f82bb9d93/video/c88b277171a94d9c3ac7e32f82bb9d93.m3u8",
-          },
-        ],
-      },
-    };
-  },
-};
+	export default {
+		name: "Player",
+		data() {
+			return {
+				playerOptions: {
+					preload: "auto",
+					autoplay: true,
+					muted: true,
+					loop: true,
+					aspectRatio: "4:3",
+					sources: [
+						{
+							type: "application/x-mpegURL",
+							src: "http://d384sk7z91xokb.cloudfront.net/output/video/2022.02.06/c88b277171a94d9c3ac7e32f82bb9d93/video/c88b277171a94d9c3ac7e32f82bb9d93.m3u8",
+						},
+					],
+				},
+			};
+		},
+	};
 </script>
 
-<style>
-</style>
+<style></style>
