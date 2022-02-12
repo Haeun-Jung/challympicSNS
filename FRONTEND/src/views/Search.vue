c09150a865ff3a75ae6649f824495c6a1e7332cd
june_25 <hp0006@mix.wvu.edu>
2022-02-13 06:39:19 +0900

    build<FE>: 동영상 재생 문제로, 포스트와 챌린지 결과의 라우터를 나누었습니다.

---
+++ FRONTEND/src/views/Search.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-app class="search-result-container">
+		<router-view />
+	</v-app>
+</template>
+
+<script>
+	export default {
+		name: "Search",
+	};
+</script>
+
+<style>
+	.search-result-container {
+		padding-left: 0%;
+	}
+	.search-control-box-mobile {
+		font-weight: bold;
+	}
+	.search-control-box-desktop {
+		font-weight: bold;
+	}
+	.search-control-box-mobile #search-keyword {
+		color: #3396f4;
+		margin-right: 1.25em;
+		font-size: 1.5rem !important;
+	}
+	.search-control-box-desktop #search-keyword {
+		color: #3396f4;
+		margin-right: 1.25em;
+		font-size: 1.5rem !important;
+	}
+</style>
