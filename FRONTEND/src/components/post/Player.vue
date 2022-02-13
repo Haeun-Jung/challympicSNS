a2eb2ca0bcd5a55c0dce856d1c1350062a494cbf
yujin won <won_yujin@naver.com>
2022-02-13 17:30:53 +0900

    fix<FE>: postItem 비디오 재생 에러 해결

--- FRONTEND/src/components/post/Player.vue
+++ FRONTEND/src/components/post/Player.vue
@@ -18,22 +18,26 @@ export default {
   },
   data() {
     return {
-      playerOptions: {
-        preload: "auto",
-        autoplay: true,
-        muted: true,
-        loop: true,
-        aspectRatio: "4:3",
-        sources: [
-          {
-            type: "application/x-mpegURL",
-            src: "http://d384sk7z91xokb.cloudfront.net/output/media/20220209/092119/video/c88b277171a94d9c3ac7e32f82bb9d93.m3u8",
-            // src: `https://d384sk7z91xokb.cloudfront.net/${this.file_path}/video/${this.fileName}.m3u8`,
-          },
-        ],
-      },
+      playerOptions: {},
     };
   },
+  created() {
+    this.playerOptions = {
+      preload: "auto",
+      autoplay: true,
+      muted: true,
+      loop: true,
+      aspectRatio: "4:3",
+      sources: [
+        {
+          type: "application/x-mpegURL",
+          // src: "http://d384sk7z91xokb.cloudfront.net/output/media/20220209/092119/video/c88b277171a94d9c3ac7e32f82bb9d93.m3u8",
+          src: `https://d384sk7z91xokb.cloudfront.net/${this.filePath}/video/${this.fileName}.m3u8`,
+        },
+      ],
+    }
+     console.log(this.playerOptions["sources"]);
+  }
 };
 </script>
 
